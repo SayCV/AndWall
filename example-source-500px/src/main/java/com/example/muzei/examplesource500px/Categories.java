440dec6fa689ee050ee08a11e6ff1fdc04290f35
SayCV <sssxyc@163.com>
2015-02-05 14:41:07 +0800

    add Categories.java

---
+++ example-source-500px/src/main/java/com/example/muzei/examplesource500px/Categories.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.muzei.examplesource500px;
+
+public interface Categories {
+    // All Categories
+    public static final String Abstract = "Abstract";
+    public static final String Macro = "Macro";
+    public static final String Animals = "Animals";
+    public static final String Nature = "Nature";
+    public static final String BlackAndWhite = "Black and White";
+    public static final String People = "People";
+    public static final String Celebrities = "Celebrities";
+    public static final String PerformingArts = "Performing Arts";
+    public static final String CityAndArchitecture = "City & Architecture";
+    public static final String Sport = "Sport";
+    public static final String Commercial = "Commercial";
+    public static final String Still Life = "Still Life";
+    public static final String Concert = "Concert";
+    public static final String Street = "Street";
+    public static final String Family = "Family";
+    public static final String Transportation = "Transportation";
+    public static final String Fashion = "Fashion";
+    public static final String Travel = "Travel";
+    public static final String Film = "Film";
+    public static final String Underwater = "Underwater";
+    public static final String FineArt = "Fine Art";
+    public static final String UrbanExploration = "Urban Exploration";
+    public static final String Food = "Food";
+    public static final String Wedding = "Wedding";
+    public static final String Journalism = "Journalism";
+    public static final String Uncategorized = "Uncategorized";
+    public static final String Landscapes = "Landscapes";
+}
